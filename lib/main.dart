b3462024b43292d5b43c88bf6c426327c9849c24
HenriqueHartmann <alemaohartmann@gmail.com>
2022-06-15 20:57:09 -0300

    upload parcial project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:bytebank/screens/dashboard.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await firebase.initializeApp();
+
   runApp(const BytebankApp());
 }
 
